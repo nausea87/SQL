d0ac07c514b1527e7bc11c4cb5d953a66ae6dd93
Erik <62840687+nausea87@users.noreply.github.com>
2020-10-22 15:01:58 +0200

    sql

---
+++ murdermystery.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS 'tabell namn'(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+)
+
+
+/* Background
+
+Sandra has been murderererered. Oh nooes. /sadface.
+
+
+
+The body was found at Zamenhof, Gothenburg (crime scene)
+and there are x amount of (witnesses)
+
+-SNACKA MED VITTNEN- (testimonies)
+   
+   Gilda: "Såg att Vincent hade bråttom härifrån",
+   
+   Linn: "Erik såg änna jävligt suspekt ut.",
+   
+   : "Min mormor har en hund. Men såg Gilda springa där ifrån",
+   
+   Daniel: "Jag misstänker Linn. Men såg också att Gilda hade blod på benen",
+   
+  Simon: "Strax efteråt så sprang Emil och Ida snabbt               härifrån",
+   
+   
+   
+   
+
+   
+   Whole class at Zamenhof (suspects)
+   Vincent, Erik, Gilda, Gilda, Emil, Ida
+   
+   
+   
+   
+   
+   
+   
